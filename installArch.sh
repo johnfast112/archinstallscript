24ab62c58c235fb8a66d2af03b0ee7e08166ef5c
johnfast112 <128560355+johnfast112@users.noreply.github.com>
2023-12-14 20:50:53 -0700

    Create installArch.sh

---
+++ installArch.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+pacman -Sy archlinux-keyring --noconfirm
+mkfx.ext4 /dev/sda2
+mkswap /dev/sda1
+swapon /dev/sda1 
+mount /dev/sda2 /mnt
+pacstrap -K /mnt base linux linux-firmware neovim neofetch git grub sudo networkmanager
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt /bin/bash \
+  -c "ln -sf /usr/share/zoneinfo/Canada/Mountain /etc/localtime && \
+  hwclock --systohc && \
+  sed -i 's/##en_US/en_US/g' /etc/locale.gen && \
+  locale-gen && \
+  echo LANG=en_US.UTF-8 >> /etc/locale.conf && \
+  echo rtx4080-arch-vm >> /etc/hostname && \
+  mkinitcpio -P && \
+  grub-install /dev/sda --target=i386-pc && \
+  systemctl enable NetworkManager && \
+  grub-mkconfig -o /boot/grub/grub.cfg && \
+  passwd root "
